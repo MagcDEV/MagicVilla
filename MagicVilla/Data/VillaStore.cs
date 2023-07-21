d16c0545ba1889f9a3d756957a216f7b3fa3ad3b
MagcDev <manuel117@gmail.com>
2023-07-21 18:15:26 -0400

    Set up restfull api

---
+++ MagicVilla/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla.Models.Dto;
+
+namespace MagicVilla.Data
+{
+    public class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> { 
+            new VillaDTO { Id=1, Name="Pool View", Ocupancy = 4, Sqft = 40},
+            new VillaDTO { Id = 2, Name = "Beach View", Ocupancy = 4, Sqft = 40 }
+        };
+    }
+}
